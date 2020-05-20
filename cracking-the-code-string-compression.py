8f8d4f4e977a1d9a7a50d791d0164947de64f415
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-20 12:53:42 -0700

    Include github python solution for Cracking the Coding Interview problem: String Compression

--- cracking-the-code-string-compression.py
+++ cracking-the-code-string-compression.py
@@ -9,6 +9,8 @@
 #     -> 'l1m1a1o3'
 # output: 'lmaooo'
 
+# My Attempt
+
 def string_compression(string):
     #keep track with a letter_counter
     #new_string variable 
@@ -48,6 +50,63 @@ def string_compression(string):
         return string
     return new_string
 
+
+print(string_compression('aabcccccaaa'))
+print(string_compression('lmaooo'))
+print(string_compression('yoooooooab'))
+
+
+
+# Cracking the Coding Interview Python Solution
+
+'''
+Solution:
+1. The compressed length of a string can be checked in linear time without actually allocating
+a new string. Thus, check compressed length by counting the number of identical consecutive characters.
+
+2. If the compressed length of the string is lower than the normal length of the string, allocate a new string
+of length equal to the compressed length, and repeat the procedure in (1) with an added step to populate the
+new string while the original string is being traversed.
+
+In Python, strings are immutable so the allocation will not happen all at once because individual characters
+of a string can't be edited. The compressed string will be initialized with the first character of the uncompressed
+string and appended to.
+
+Time complexity: O(N) where N is the length of the shorter string.
+Space complexity: O(N).
+'''
+
+def compute_compressed_length(string):
+    if string is None:
+        return 0
+    if len(string) == 1:
+        return 2
+    compressed_length = 1
+    num_dupes = 1
+    for i in range(1, len(string)):
+        if string[i] == string[i - 1]:
+            num_dupes += 1
+        else:
+            compressed_length += (1 + len(str(num_dupes)))  # increase length by 1 letter + number of digits in num_dupes
+            num_dupes = 1
+    return compressed_length
+
+
+def string_compression(string):
+    if string is None:
+        return None
+    if len(string) <= compute_compressed_length(string):
+        return string
+    compressed = string[0]
+    num_dupes = 1
+    for i in range(1, len(string)):
+        if string[i] == string[i-1]:
+            num_dupes += 1
+        else:
+            compressed += str(num_dupes) + string[i]
+            num_dupes = 1
+    compressed += str(num_dupes)
+    return compressed
 
 print(string_compression('aabcccccaaa'))
 print(string_compression('lmaooo'))

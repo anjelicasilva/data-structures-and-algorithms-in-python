07db0c89e590df0bf9ce87d5f075b511194246b0
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-20 12:51:45 -0700

    Add cracking the coding interview problem: String Compression

---
+++ cracking-the-code-string-compression.py
@@ -0,0 +1,12 @@
+
+# Implement a method to perform basic string compression using the counts of repeted characters.
+# If the "compressed" string would not become smaller than the original string, 
+# your method should return the original string.
+
+# input: 'aabcccccaaa'
+# output: 'a2b1c5a3'
+# input: 'lmaooo'
+#     -> 'l1m1a1o3'
+# output: 'lmaooo'
+
+def string_compression(string):
